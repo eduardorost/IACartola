4c9c23c7e23dd96c0c06d72a8cae15439cc3413f
Sacha Kilinski <flopflopstudios@gmail.com>
2015-05-05 09:00:10 -0300

    broken implementation - to be fixed soon

--- cartola_scrapper/cartola_scrapper.rb
+++ cartola_scrapper/cartola_scrapper.rb
@@ -1,13 +1,29 @@
 require 'net/http'
 require 'json'
 
-class Jogador
-
-end
-
 class CartolaScrapper
-  uri = URI('http://cartolafc.globo.com/mercado/filtrar.json')
-  j =  JSON.parse(Net::HTTP.get(uri))
-  p j
+
+  @@uri =  URI("http://cartolafc.globo.com/mercado/filtrar.json")
+
+  @@players = []
+
+  def self.get_total_pages
+    JSON.parse(Net::HTTP.get(@@uri))["page"]["total"]
+  end
+
+  def self.get_rodada
+    JSON.parse(Net::HTTP.get(@@uri))["rodada_id"]
+  end
+
+  for i in 1..get_total_pages.to_i
+    params = { :page => i }
+    @@uri.query = URI.encode_www_form(params)
+    p JSON.parse(Net::HTTP.get_response(@@uri))
+    @@players.concat(JSON.parse(Net::HTTP.get_response(@@uri))["atletas"])
+    p @@players.size
+  end
+
 end
+
+
 
